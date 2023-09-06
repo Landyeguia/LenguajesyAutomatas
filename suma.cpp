de1e1fba9526c785de18d6451b82e2b174db6250
Landyeguia <124419160+Landyeguia@users.noreply.github.com>
2023-09-06 13:30:57 -0600

    Add files via upload

---
+++ suma.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+#include<iostream>
+#include<conio.h>
+
+float a,b,c,d;
+
+void main() // Funcion principal
+{
+    a=(3+5)*8-(10-4)/2;
+    b=8;
+    printf("Valor de c = ");
+    scanf("%f",&c);
+    if (1==1)
+    {
+        if (1==1)
+            if (2==2)
+                if (3==3)
+                    if (4==4)
+                        printf("1 == 2");
+    }
+}
